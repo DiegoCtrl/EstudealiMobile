647255b2826cb79fe2ff9a6f03430f914c36c451
Diego Araújo <118080825+DiegoCtrl@users.noreply.github.com>
2023-06-08 21:32:30 -0300

    Primeiro upload de tela, apenas para marcar inicio
    tela de cursos curtidos EstudeAlí

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <view style={styles.back}></view>
+      <Text>Aqui vão ficar os cursos curtidos</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  back:{
+    backgroundColor: 'green', 
+    height: 500,
+    width: 500,
+  }
+});
